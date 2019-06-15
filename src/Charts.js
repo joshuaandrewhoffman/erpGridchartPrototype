bde2f7bf6a0ef597356966d2c1579574ec68a710
Josh Hoffman <josh@fitabase.com>
2019-06-15 16:21:10 -0700

    add a component for the charts and grid

---
+++ src/Charts.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+import './App.css';
+import { VictoryPie } from "victory";
+
+function Charts() {
+  return (
+    <div className="Charts">
+
+      <VictoryPie />
+          </div>
+  );
+}
+
+export default Charts;
---
+++ src/Grid.js
@@ -0,0 +1,25 @@
+import React, {Component} from 'react';
+import './App.css';
+import ReactDataGrid from 'react-data-grid';
+
+function Grid() {
+  const columns = [
+    { key: 'id', name: 'ID' },
+    { key: 'title', name: 'Title' },
+    { key: 'count', name: 'Count' } ];
+  
+  const rows = [{id: 0, title: 'row1', count: 20}, {id: 1, title: 'row1', count: 40}, {id: 2, title: 'row1', count: 60}];
+  
+  
+  return (
+    <div className="Grid">
+      <ReactDataGrid
+        columns={columns}
+        rowGetter={i => rows[i]}
+        rowsCount={3}
+        minHeight={150} />
+    </div>
+  );
+}
+
+export default Grid;
